93dd1f5fa6c69997cb59d7759552a3c77247535f
Justin Merrell <merrelljustin@gmail.com>
2024-01-26 22:02:40 -0500

    first commit

--- Dockerfile
+++ Dockerfile
@@ -26,4 +26,6 @@ RUN python3.11 -m pip install --upgrade pip && \
 # Add src files (Worker Template)
 ADD src .
 
+RUN python3.11 /handler.py
+
 CMD python3.11 -u /handler.py
--- src/handler.py
+++ src/handler.py
@@ -1,18 +1,32 @@
 """ Example handler file. """
 
 import runpod
+from diffusers import AutoPipelineForText2Image
+import torch
+import base64
+import io
+import time
 
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless.
 
+pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
+pipe.to("cuda")
 
 def handler(job):
     """ Handler function that will be used to process jobs. """
     job_input = job['input']
+    prompt = job_input['prompt']
 
-    name = job_input.get('name', 'World')
+    time_start = time.time()
+    image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+    print(f"Time taken: {time.time() - time_start}")
 
-    return f"Hello, {name}!"
+    buffer = io.BytesIO()
+    image.save(buffer, format="PNG")
+    image_bytes = buffer.getvalue()
+
+    return base64.b64encode(image_bytes).decode('utf-8')
 
 
 runpod.serverless.start({"handler": handler})
