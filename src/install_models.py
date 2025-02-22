c61666783eed7fa96a39017af11e4f734a38babe
royalsoftkv <38809329+royalsoftkv@users.noreply.github.com>
2025-02-22 14:20:37 +0100

    Update install_models.py

--- src/install_models.py
+++ src/install_models.py
@@ -5,13 +5,11 @@ import torch
 model_name='SG161222/RealVisXL_V4.0'
 #model_name='SG161222/RealVisXL_V5.0'
 
-vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix")
 pipe = AutoPipelineForText2Image.from_pretrained(
     model_name,
     vae=vae,
-    torch_dtype=torch.float16,
     use_safetensors=True,
     add_watermarker=False,
-    variant="fp16",
     custom_pipeline="lpw_stable_diffusion_xl"
 )
